a0ae63fcfdd0bae6f48c043eb1a3286e6706dd4b
Nikiodoz <niki.tamjidi@gmail.com>
2016-05-10 11:14:19 +0200

    Comment

---
+++ app/src/androidTest/java/dk/kea/androidclass2016/sk8runner/sk8runner/ApplicationTest.java
@@ -0,0 +1,15 @@
+package dk.kea.androidclass2016.sk8runner.sk8runner;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application>
+{
+    public ApplicationTest()
+    {
+        super(Application.class);
+    }
+}
---
+++ app/src/test/java/dk/kea/androidclass2016/sk8runner/sk8runner/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package dk.kea.androidclass2016.sk8runner.sk8runner;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest
+{
+    @Test
+    public void addition_isCorrect() throws Exception
+    {
+        assertEquals(4, 2 + 2);
+    }
+}
