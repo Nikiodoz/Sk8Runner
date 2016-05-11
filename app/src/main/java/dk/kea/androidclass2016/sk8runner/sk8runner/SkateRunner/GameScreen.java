5bc39c4cf557023e838cb4f98726603ce92fda74
mistochan <eliasdavidsen@hotmail.com>
2016-05-11 14:55:55 +0200

    GameScreen Update

--- app/src/main/java/dk/kea/androidclass2016/sk8runner/sk8runner/SkateRunner/GameScreen.java
+++ app/src/main/java/dk/kea/androidclass2016/sk8runner/sk8runner/SkateRunner/GameScreen.java
@@ -3,10 +3,13 @@ package dk.kea.androidclass2016.sk8runner.sk8runner.SkateRunner;
 import android.graphics.Bitmap;
 import android.graphics.Typeface;
 
+import java.util.List;
+
 import dk.kea.androidclass2016.sk8runner.sk8runner.Game;
 import dk.kea.androidclass2016.sk8runner.sk8runner.Music;
 import dk.kea.androidclass2016.sk8runner.sk8runner.Screen;
 import dk.kea.androidclass2016.sk8runner.sk8runner.Sound;
+import dk.kea.androidclass2016.sk8runner.sk8runner.TouchEvent;
 
 /**
  * Created by Ahmed on 11-05-2016.
@@ -43,6 +46,26 @@ public class GameScreen extends Screen
     public void update(float deltaTime)
     {
 
+     //       if (world.gameOver == true) state = State.GameOver;
+            if(state == State.Paused && game.getTouchEvents().size() > 0);
+        {
+            state = State.Running;resume();
+        }
+               if (state == State.GameOver)
+        {
+            List<TouchEvent> events = game.getTouchEvents();
+          int stop = events.size();
+            for (int i = 0; i < stop; i++) {
+               if (events.get(i).type == TouchEvent.TouchEventType.Up)
+               {
+                   game.setScreen(new MenuScreen(game));
+                   return;
+                }
+            }
+
+            game.setScreen(new MenuScreen(game));
+            return;
+        }
     }
 
     @Override
