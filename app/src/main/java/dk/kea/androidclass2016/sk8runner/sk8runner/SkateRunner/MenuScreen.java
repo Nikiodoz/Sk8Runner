f7f7144007d9b183ddeb9fa2cf4b83bcb9d67246
Nikiodoz <niki.tamjidi@gmail.com>
2016-05-11 12:58:37 +0200

    More fixed packages
    fixed packages in menuscreen class and rungame class

--- app/src/main/java/dk/kea/androidclass2016/sk8runner/sk8runner/SkateRunner/MenuScreen.java
+++ app/src/main/java/dk/kea/androidclass2016/sk8runner/sk8runner/SkateRunner/MenuScreen.java
@@ -2,9 +2,10 @@ package dk.kea.androidclass2016.sk8runner.sk8runner.SkateRunner;
 
 import android.graphics.Bitmap;
 
-import dk.kea.patrick.firstapp.BreakoutGame.GameScreen;
-import dk.kea.patrick.firstapp.Game;
-import dk.kea.patrick.firstapp.Screen;
+import dk.kea.androidclass2016.sk8runner.sk8runner.BreakoutGame.GameScreen;
+import dk.kea.androidclass2016.sk8runner.sk8runner.Game;
+import dk.kea.androidclass2016.sk8runner.sk8runner.Screen;
+
 
 /**
  * Created by Ahmed on 09-05-2016.
