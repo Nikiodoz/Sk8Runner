8777a0f96ba61589b6ab1a2c883ec397dec45817
Nikiodoz <niki.tamjidi@gmail.com>
2016-05-18 20:34:23 +0200

    character changes
    not working yet

---
+++ app/src/main/java/dk/kea/androidclass2016/sk8runner/sk8runner/SkateRunner/Character.java
@@ -0,0 +1,12 @@
+package dk.kea.androidclass2016.sk8runner.sk8runner.SkateRunner;
+
+/**
+ * Created by Niki Tamjidi on 11-05-2016.
+ */
+public class Character
+{
+    public static final float WIDTH = 56;
+    public static final float HEIGHT = 11;
+    float x = 16 - WIDTH/2;
+    float y = World.MAX_Y - 30;
+}
